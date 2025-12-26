95688fe908786ca7b47cd8fbd056e3c60c812909
onlysahilrahman <onlysahilrahman@gmail.com>
2025-12-26 16:21:19 +0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const btn = document.querySelector('.flip-btn');
+const card = document.querySelector('.inner-container');
+
+btn.addEventListener('click', () => {
+  card.classList.add('flip-active');
+
+  setTimeout(() => {
+    window.location.href =
+      "https://www.effectivegatecpm.com/gykzahp34?key=7d4225f2e819c0b5950e4090b0b106e8";
+  }, 4000);
+});
